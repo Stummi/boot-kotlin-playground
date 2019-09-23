bfe4413e6a0ad87f089dccfa00b5e6de51ff4a2c
Michael Stummvoll <michael.stummvoll@staffbase.com>
2019-09-23 15:57:15 +0200

    fix

--- src/main/kotlin/org/stummi/hellokotlin/HelloController.kt
+++ src/main/kotlin/org/stummi/hellokotlin/HelloController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/")
 class HelloController(val helloService: HelloService) {
-
     @GetMapping(path = ["hello/{name}"])
     fun helloWorld(@PathVariable("name") name: String) = helloService.greet(name)
 }
